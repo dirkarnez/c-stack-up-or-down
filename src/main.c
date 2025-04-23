63c60c931d547d0612fc0697e85190283347a48d
Dirk Arnez <smalldirkalex@gmail.com>
2025-04-24 03:57:10 +0800

    - update src/main.c

--- src/main.c
+++ src/main.c
@@ -11,7 +11,22 @@ bool stack_is_up_or_down(int *other) {
     }
 }
 
+#ifdef __AVR_ATmega328P__
+void setup() {
+  Serial.begin(115200); 
+}
+
+void loop() {
+  Serial.println(stack_is_up_or_down(NULL) ? "UP" : "DOWN"); //Down
+  delay(1000); 
+}
+#else
+
 int main() {
     printf("%s\n", stack_is_up_or_down(NULL) ? "UP" : "DOWN");
     return 0;
 }
+
+#endif
+
+
